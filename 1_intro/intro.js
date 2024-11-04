5fffb941ab63091962e219528a5ddd33640a766e
vigneshwaran p k <96305123+mrsmallboy@users.noreply.github.com>
2024-11-04 15:13:46 +0530

    Add files via upload

---
+++ 1_intro/intro.js
@@ -0,0 +1,30 @@
+
+console.log("hello i am external javascript file");
+// node js is for run time environment
+//node filename.js
+// type of variables
+// var,let,const
+let num1=10
+var num2=20
+const num3=30
+console.log(num1);
+console.log(num2);
+console.log(num3);
+var num2=100010
+//let num2=300
+// different between var and let 
+// in var we can redeclare the variable
+// in let we can't redeclare the variable
+var str
+str="i am string1"
+let str1
+str1="hello"
+console.log(str);
+console.log(str1);
+// const c
+//c=40
+// variable declaration and initialization we have to do in single line for the const
+//! why dynamic 
+let str2=10
+str2="new string"
+console.log(str2);
